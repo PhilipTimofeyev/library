059e611804ce9ff70ba67a5029fbd4b2fbe5ef49
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-08-13 10:09:58 -0700

    Change Library and Book to classes

--- js/libraryscript.js
+++ js/libraryscript.js
@@ -1,55 +1,114 @@
-const donQuixote = new Book("Don Quixote", "Miguel de Cervantes", 1072, 'Read')
-const warAndPeace = new Book("War and Peace", "Leo Tolstoy", 1296, 'Read')
 
-const myLibrary = [donQuixote, warAndPeace];
+// const myLibrary = [donQuixote, warAndPeace];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+class Book {
 
-function addBookToLibrary(book) {
-  myLibrary.push(book)
-}
-
-function showBooks() {
-  let temp = document.getElementsByTagName("template")[0];
-  let item = temp.content
-
-  resetShownBooks()
-
-  for (const book of myLibrary) {
-    let tempCopy = document.importNode(item, true);
-    let removeBookBtn = tempCopy.querySelector(".removeBtn");
-    let changeReadBtn = tempCopy.querySelector(".readBtn");
-
-    tempCopy.querySelector(".title").innerText = `${book.title}`;
-    tempCopy.querySelector(".author").innerText = `by ${book.author}`;
-    tempCopy.querySelector(".pages").innerText = `${book.pages} pages`;
-    tempCopy.querySelector(".read").innerText = `${book.read}`;
-
-    changeReadBtn.addEventListener('click', function () {
-      toggleRead(book)
-      this.parentNode.parentNode.querySelector(".read").innerText = `${book.read}`;
-    });
-
-    document.querySelector(".library").appendChild(tempCopy);
-
-    removeBookBtn.addEventListener('click', function () {
-      bookIndex = myLibrary.findIndex((element) => element.title == book.title);
-      myLibrary.splice(bookIndex, 1)
-      this.parentNode.parentNode.remove()
-    });
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
   }
 }
 
+class Library {
+
+  constructor() {
+    this.bookShelf = [donQuixote, warAndPeace]
+  }
+
+  addBook(book) {
+    console.log(this.bookShelf)
+    this.bookShelf.push(book)
+  }
+
+
+  showBooks() {
+    let temp = document.getElementsByTagName("template")[0];
+    let item = temp.content
+    let bookShelf = this.bookShelf
+
+
+    resetShownBooks()
+
+    for (const book of this.bookShelf) {
+      let tempCopy = document.importNode(item, true);
+      let removeBookBtn = tempCopy.querySelector(".removeBtn");
+      let changeReadBtn = tempCopy.querySelector(".readBtn");
+
+      tempCopy.querySelector(".title").innerText = `${book.title}`;
+      tempCopy.querySelector(".author").innerText = `by ${book.author}`;
+      tempCopy.querySelector(".pages").innerText = `${book.pages} pages`;
+      tempCopy.querySelector(".read").innerText = `${book.read}`;
+
+      changeReadBtn.addEventListener('click', function () {
+        toggleRead(book)
+        this.parentNode.parentNode.querySelector(".read").innerText = `${book.read}`;
+      });
+
+      document.querySelector(".library").appendChild(tempCopy);
+
+      removeBookBtn.addEventListener('click', function () {
+        let bookIndex = bookShelf.findIndex((element) => element.title == book.title);
+        bookShelf.splice(bookIndex, 1)
+        this.parentNode.parentNode.remove()
+      });
+    }
+  }
+}
+
+
+const donQuixote = new Book("Don Quixote", "Miguel de Cervantes", 1072, 'Read')
+const warAndPeace = new Book("War and Peace", "Leo Tolstoy", 1296, 'Read')
+
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+// }
+
+// function addBookToLibrary(book) {
+//   myLibrary.push(book)
+// }
+
+// function showBooks() {
+//   let temp = document.getElementsByTagName("template")[0];
+//   let item = temp.content
+
+//   resetShownBooks()
+
+//   for (const book of myLibrary) {
+//     let tempCopy = document.importNode(item, true);
+//     let removeBookBtn = tempCopy.querySelector(".removeBtn");
+//     let changeReadBtn = tempCopy.querySelector(".readBtn");
+
+//     tempCopy.querySelector(".title").innerText = `${book.title}`;
+//     tempCopy.querySelector(".author").innerText = `by ${book.author}`;
+//     tempCopy.querySelector(".pages").innerText = `${book.pages} pages`;
+//     tempCopy.querySelector(".read").innerText = `${book.read}`;
+
+//     changeReadBtn.addEventListener('click', function () {
+//       toggleRead(book)
+//       this.parentNode.parentNode.querySelector(".read").innerText = `${book.read}`;
+//     });
+
+//     document.querySelector(".library").appendChild(tempCopy);
+
+//     removeBookBtn.addEventListener('click', function () {
+//       bookIndex = myLibrary.findIndex((element) => element.title == book.title);
+//       myLibrary.splice(bookIndex, 1)
+//       this.parentNode.parentNode.remove()
+//     });
+//   }
+// }
+
 const showBooksBtn = document.getElementById("showBooksBtn")
 const removeBookBtn = document.querySelector(`[data-remove="${document.id}"]`)
+const myLibrary = new Library
 
 showBooksBtn.addEventListener("click", (event) => {
-  showBooks()
+  myLibrary.showBooks()
 })
 
 let addBookBtn = document.querySelector('[data-show-modal]');
